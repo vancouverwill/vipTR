56545e1d0c121f0577f76ad4acf1551a53bf61ef
William Melbourne <will_melbourne@hotmail.com>
2012-04-07 20:30:37 -0700

    add theme changes

---
+++ wp-content/themes/vipElectrical/HomeSingleSideBarPage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Home Singe Sidebar Template
+ * Description: A Page Template that adds a sidebar to pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" class="homeContent" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+                        <div class="homeSidebar">
+                            <div class="pagedivider">&nbsp;</div>
+                            <a href=""><img src="/wp-content/uploads/TEXT_AREA_EMPLOYMENT_BOX.jpg"></a>
+                            <a href=""><img src="/wp-content/uploads/TEXT_AREA_CAPABILITY_BOX.jpg"></a>
+                        </div>
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
