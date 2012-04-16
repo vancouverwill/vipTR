be1a45817d7ed639df10d04c87744e32e5dbce10
William Melbourne <will_melbourne@hotmail.com>
2012-04-15 23:05:59 -0700

    all good

--- wp-content/themes/vipElectrical/functions.php
+++ wp-content/themes/vipElectrical/functions.php
@@ -18,4 +18,24 @@ function vipelectrical_body_class( $class = '' ) {
 	// Separates classes with a single space, collates classes for body element
 	echo 'class="' . join( ' ', get_body_class( $class ) ) . '"';
 }
-?>
+
+
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own twentyeleven_posted_on to override in a child theme
+ *
+ * @since Twenty Eleven 1.0
+ */
+function twentyeleven_posted_on() {
+	printf( __( '<span class="sep"></span>
+		<a href="%1$s" title="%2$s" rel="bookmark">
+		-<time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'twentyeleven' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		esc_attr( sprintf( __( 'View all posts by %s', 'twentyeleven' ), get_the_author() ) ),
+		get_the_author()
+	);
+}
