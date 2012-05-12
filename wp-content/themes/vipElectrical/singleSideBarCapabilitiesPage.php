f190bc44c7f631287ad32e22dd03151ba7aeb451
will working on the 247 server <will_melbourne@hotmail.com>
2012-05-12 12:00:11 -0700

    added capapabilities and markets page

---
+++ wp-content/themes/vipElectrical/singleSideBarCapabilitiesPage.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Singe Sidebar Capabilties Template
+ * Description: A Page Template that adds a sidebar to pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+
+
+		<div id="primary" class="singleSideBarComittmentPage">
+			<?php
+$submenu = hierarchical_submenu($post, 'comittmentMenu');
+if ($submenu) {
+    echo $submenu;
+} else {
+	echo '<p>nothing to display</p>';
+    // Do something else
+}
+?>
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>  
