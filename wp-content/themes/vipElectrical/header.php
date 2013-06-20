08626f73301e4095295a675e6e0aae58a7c2db03
will working on the 247 server <will_melbourne@hotmail.com>
2013-06-20 09:15:17 -0700

    getting close to ready on carousel

--- wp-content/themes/vipElectrical/header.php
+++ wp-content/themes/vipElectrical/header.php
@@ -147,7 +147,165 @@
             $image_url=$image_url[0];
 
             ?>
+            <?php $post = $wp_query->get_queried_object();
+                    $pagename = $post->post_name; 
+            if ($pagename != 'home') { ?>
+
             <div id="main">
                 <div id="mainFold">&nbsp;</div>
                <!-- <div class="mainContentImage" style="background:url(<? echo $image_url; ?>);">&nbsp;</div> -->
                <img class="mainContentImage" src="<?php echo $image_url; ?>" />
+            <? } else { ?>
+                
+               <script type="text/javascript">
+
+                    //rotation speed and timer
+                    var speed = 3000;
+                    var run = setInterval('rotate()', speed);
+
+
+                  jQuery(document).ready(function() {
+                        //move he last list item before the first item. The purpose of this is if the user clicks to slide left he will be able to see the last item.
+                        jQuery('#carousel_ul li:first').before(jQuery('#carousel_ul li:last')); 
+                        
+                        
+                        //when user clicks the right image for background sliding left        
+                        jQuery('#right_scroll img').click(function(){
+                        
+                            //get the width of the items ( i like making the jquery part dynamic, so if you change the width in the css you won't have o change it here too ) '
+                            var item_width = jQuery('#carousel_ul li').outerWidth() + 10;
+
+                            console.log(jQuery('#carousel_ul').css('left'));
+                            console.log(item_width);
+                            
+                            //calculae the new left indent of the unordered list
+                            var left_indent = parseInt(jQuery('#carousel_ul').css('left')) - item_width;
+
+                            console.log(left_indent);
+                            
+                            //make the sliding effect using jquery's anumate function '
+                            jQuery('#carousel_ul:not(:animated)').animate({'left' : left_indent},1000,function(){    
+                                
+                                //get the first list item and put it after the last list item (that's how the infinite effects is made) '
+                                jQuery('#carousel_ul li:last').after(jQuery('#carousel_ul li:first')); 
+                                
+                                //and get the left indent to the default -210px
+                                jQuery('#carousel_ul').css({'left' : '-' + item_width + 'px'});
+                            }); 
+                        });
+                        
+                        //when user clicks the left image for background sliding left
+                        jQuery('#left_scroll img').click(function(){
+                            
+                            var item_width = jQuery('#carousel_ul li').outerWidth() + 10;
+                            
+                            /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */
+                            var left_indent = parseInt(jQuery('#carousel_ul').css('left')) + item_width;
+                            
+                            jQuery('#carousel_ul:not(:animated)').animate({'left' : left_indent},1000,function(){    
+                            
+                            /* when sliding to left we are moving the last item before the first list item */            
+                            jQuery('#carousel_ul li:first').before(jQuery('#carousel_ul li:last')); 
+                            
+                            /* and again, when we make that change we are setting the left indent of our unordered list to the default -210px */
+                            jQuery('#carousel_ul').css({'left' : '-' + item_width + 'px'});
+                            });
+                            
+                            
+                        });
+                  });
+
+                    //a simple function to click next link
+                    //a timer will call this function, and the rotation will begin :)  
+                    function rotate() {
+                        jQuery('#right_scroll img').click();
+                    }
+                </script>
+<style type="text/css">
+#carousel_container {
+    position:relative;
+}
+
+#carousel_inner {
+float:left; /* important for inline positioning */
+width:901px; /* important (this width = width of list item(including margin) * items shown */ 
+overflow: hidden;  /* important (hide the items outside the div) */
+/* non-important styling bellow */
+background: #F0F0F0;
+
+/* wills editions */
+ osition: relative;
+ -index: -10;
+}
+
+#carousel_ul {
+position:relative;
+left:-901px; /* important (this should be negative number of list items width(including margin) */
+list-style-type: none; /* removing the default styling for unordered list items */
+margin: 0px;
+padding: 0px;
+width:9999px; /* important */
+/* non-important styling bellow */
+padding-bottom:10px;
+}
+
+#carousel_ul li{
+float: left; /* important for inline positioning of the list items */                                    
+width: 901px;  /* fixed width, important */
+/* just styling bellow*/
+padding:0px;
+height:338px;
+background: #000000;
+margin-top:0px;
+margin-bottom:0px; 
+margin-left:0px; 
+margin-right:0px; 
+}
+
+#carousel_ul li img {
+.margin-bottom:-4px; /* IE is making a 4px gap bellow an image inside of an anchor (<a href...>) so this is to fix that*/
+/* styling */
+cursor:pointer;
+cursor: hand; 
+border:0px; 
+}
+#left_scroll, #right_scroll{
+float:left; 
+height:32px; 
+width:32px; 
+background: #C0C0C0; 
+position:absolute;
+top: 100px;
+z-index:20px;
+}
+
+#left_scroll img, #right_scroll img{
+/*styling*/
+cursor: pointer;
+cursor: hand;
+}
+
+#left_scroll {
+left:0px;
+}
+
+#right_scroll {
+right:0px;
+}
+</style>
+
+                <div id='carousel_container'>
+                  <div id='left_scroll'><img src='/wp-content/themes/vipElectrical/images/home_image_button_left.png' /></div>
+                    <div id='carousel_inner'>
+                        <ul id='carousel_ul'>
+                            <li><a href='#'><img src="/wp-content/themes/vipElectrical/images/vip_home_1_image.jpg" width="901px" height="338px" /></a></li>
+                            <li><a href='#'><img src="/wp-content/themes/vipElectrical/images/vip_home_2_image.jpg" width="901px" height="338px" /></a></li>
+                            <li><a href='#'><img src="/wp-content/themes/vipElectrical/images/vip_home_3_image.jpg" width="901px" height="338px" /></a></li>
+                        </ul>
+                    </div>
+                  <div id='right_scroll'><img src='/wp-content/themes/vipElectrical/images/home_image_button_right.png' /></div>
+                </div>
+            <?php } ?>
+
+                
+
